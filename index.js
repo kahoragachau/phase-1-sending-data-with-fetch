d613c053f9ea08e3761f4dfd819caeaf6d6bde3a
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-28 18:24:22 +0300

    - Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(username, useremail){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body:JSON.stringify({name:username, email: useremail})
+    })
+    .then(response => response.json())
+    .then(data => {
+        const id = data.id;
+        document.querySelector("body").innerHTML = `${id}`
+    })
+    .catch(error =>document.querySelector("body").innerHTML = error.message )
+}
